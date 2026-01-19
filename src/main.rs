bcae35762798583f6e1166ce9718c6c88a0967f8
zhuchong <1043199504@qq.com>
2026-01-19 20:20:14 +0800

    web模版集成axum web框架

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use axum::{
+    Router, debug_handler, routing::{self}
+};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", routing::get(index));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Listening on http://localhost:3000");
+
+    axum::serve(listener, router).await.unwrap();
+}
+
+// debug_handler 这个宏axum会校验你的handler是不是符合要求，输出具体的错误信息
+#[debug_handler] 
+async fn index() -> &'static str {
+    "Hello, Rust!"
+}
